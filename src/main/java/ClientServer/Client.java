f6d39419ee3463e8f5e3f85ac63bd5cddd3a30eb
Ruby101-was-taken <abusinessemailkinda@gmail.com>
2024-04-18 14:58:38 +0000

    Client Server stuff
    -Added the Client and Server from Jakub's code
    -Added Feature 10

---
+++ src/main/java/ClientServer/Client.java
@@ -0,0 +1,122 @@
+package ClientServer;
+
+/**
+ *  Written by Jakub Polacek on 13-14.4. 2024
+ *  Used sample code from class as reference:
+ *  github.com/logued/oop-client-server-multithreaded-2024
+ *
+ *  Added to by Ruby White 18/04/2024
+ */
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+import com.google.gson.Gson;
+import DTOs.Block;
+
+
+
+public class Client
+{
+
+
+    public static Block createBlock(){
+
+        Scanner key = new Scanner(System.in);
+
+        System.out.print("Enter the name (or enter \"Close\" to exit): ");
+        String name = key.nextLine();
+
+        Block newBlock = null;
+
+        if(!name.equalsIgnoreCase("close")) {
+
+            System.out.print("Enter the hardness: ");
+            double hardness = key.nextDouble();
+
+            System.out.print("Enter the blast resistance: ");
+            double blastResistance = key.nextDouble();
+
+            System.out.print("Is gravity affected? (0 = False, 1 = True): ");
+            boolean gravityAffected = (key.nextInt() == 0) ? false : true;
+
+            newBlock = new Block(name, hardness, blastResistance, gravityAffected);
+        }
+
+
+        return newBlock;
+    }
+
+
+    public static void main(String[] args)
+    {
+        Client client = new Client();
+        client.start();
+    }
+    public void start()
+    {
+        try (Socket socket = new Socket("localhost", 7420);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())))
+        {
+            System.out.println("Client message: The Client is running and has connected to the server");
+            Scanner keyboardInput = new Scanner(System.in);
+            System.out.println("Please enter a command: ");
+            System.out.println("Available commands: F9, quit");
+            String clientCommand = keyboardInput.nextLine();
+
+            while(true)
+            {
+                if (clientCommand.equals("F9"))
+                {
+                    System.out.println("Function 9 selected - Display Block by ID");
+                    System.out.println("Please input the desired ID to be found:");
+                    clientCommand = keyboardInput.nextLine();
+                    out.println("F9"+clientCommand);
+                    String response = in.readLine();
+                    Gson gsonParser = new Gson();
+                    Block block = gsonParser.fromJson(response,Block.class);
+                    //TODO make output nicer
+                    System.out.println(block.toString());
+                }
+                // By Ruby :3
+                else if (clientCommand.equals("F10"))
+                {
+                    System.out.println("Function 10 selected - Display Block by ID");
+                    Block blockToAdd = createBlock();
+
+
+
+                    Gson gsonParser = new Gson();
+                    String blockJson = gsonParser.toJson(blockToAdd);
+
+
+                    out.println("F10"+blockJson);
+                    String response = in.readLine();
+                    //TODO make output nicer
+                    System.out.println(response);
+                }
+                else if (clientCommand.startsWith("quit"))
+                {
+                    out.println(clientCommand);
+                    String response = in.readLine();
+                    System.out.println("Client message: Response from server: \"" + response + "\"");
+                    break;
+                }
+                else
+                {
+                    System.out.println("Command unknown. Try again.");
+                }
+
+                keyboardInput = new Scanner(System.in);
+                System.out.println("Please enter a command: ");
+                clientCommand = keyboardInput.nextLine();
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Client message: IOException: " + e);
+        }
+        System.out.println("Exiting client, server may still be running.");
+    }
+}
---
+++ src/main/java/ClientServer/ClientHandler.java
@@ -0,0 +1,106 @@
+package ClientServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import DAOs.BlockDaoInterface;
+import DAOs.MySqlBlockDao;
+import DTOs.Block;
+import Exceptions.DaoException;
+import com.google.gson.Gson;
+
+/**
+ *  Written by Jakub Polacek on 13-14.4. 2024
+ *  Used sample code from class as reference:
+ *  github.com/logued/oop-client-server-multithreaded-2024
+ */
+
+public class ClientHandler implements Runnable
+{
+    BufferedReader clientReader;
+    PrintWriter clientWriter;
+    Socket clientSocket;
+    final int clientNumber;
+
+    public ClientHandler(Socket clientSocket, int clientNumber)
+    {
+        this.clientSocket = clientSocket;
+        this.clientNumber = clientNumber;
+        try
+        {
+            this.clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+            this.clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        String request;
+        BlockDaoInterface IBlockDao = new MySqlBlockDao();
+        try
+        {
+            while ((request = clientReader.readLine()) != null)
+            {
+                System.out.println("Server: (ClientHandler): Read command from client " + clientNumber + ": " + request);
+                System.out.println(request);
+                if (request.startsWith("F9"))
+                {
+                    String message = request.substring(2);
+                    String blockAsJson = IBlockDao.blockToJson(Integer.parseInt(message));
+                    clientWriter.println(blockAsJson);
+                    System.out.println("Server message: JSON string of Block by id " + message + " sent to client.");
+                }
+                else if (request.substring(0, 3).equals("F10"))
+                {
+                    String message = request.substring(3);
+                    Gson gsonParser = new Gson();
+                    Block block = gsonParser.fromJson(message,Block.class);
+                    System.out.println(message);
+                    try {
+                        IBlockDao.insertABlock(block);
+                    } catch (DaoException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    clientWriter.println("Block added.");
+                    System.out.println("Server message: JSON string of Block by id " + message + " sent to client.");
+                }
+                else if (request.startsWith("quit"))
+                {
+                    clientWriter.println("Sorry to see you leaving. Goodbye.");
+                    System.out.println("Server message: Quit request from client, executed.");
+                }
+                else
+                {
+                    clientWriter.println("Error - invalid command");
+                    System.out.println("Server message: Invalid request from client.");
+                }
+            }
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            this.clientWriter.close();
+            try
+            {
+                this.clientReader.close();
+                this.clientSocket.close();
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+        System.out.println("Server: (ClientHandler): Handler for Client " + clientNumber + " is terminating .....");
+    }
+}
